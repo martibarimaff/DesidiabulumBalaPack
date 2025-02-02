45ad7e3f35bb2ddcd71011a41e5b4a4750b4191e
martibarimaff <97847284+martibarimaff@users.noreply.github.com>
2025-02-02 13:47:07 +0100

    refactored the mod to a custom Atlas

--- DesidiabulumBalaPack.lua
+++ DesidiabulumBalaPack.lua
@@ -4,52 +4,60 @@
 
 sendDebugMessage("Launching DesidiabulumBalaPack...")
 
-SMODS.Atlas { key = "Joker", path = "JokersDDB.png", px = 71, py = 95, prefix_config = { key = false } }
+SMODS.Atlas { key = "JokersDDB", path = "JokersDDB.png", px = 71, py = 95, prefix_config = { key = true } }
 SMODS.Atlas { key = "Tarot", path = "TarotsDDB.png", px = 71, py = 95, prefix_config = { key = false } }
 SMODS.Atlas { key = "Planet", path = "TarotsDDB.png", px = 71, py = 95, prefix_config = { key = false } }
 
 ----------------------------------------------------------Jokers----------------------------------------------------------
 
 SMODS.Joker:take_ownership('j_trading', {
-	--[[atlas = 'Joker',
-	pos = { x = 9, y = 14 },]]--
+	atlas = 'JokersDDB',
+	pos = { x = 9, y = 0 },
 })
 
 SMODS.Joker:take_ownership('j_lucky_cat', {
-	--[[atlas = 'Joker',
-	pos = { x = 5, y = 14 },]]--
-})
-
-SMODS.Joker:take_ownership('j_luchador', {
-    
-})
-
-SMODS.Joker:take_ownership('j_gros_michel', {
-    
-})
-
-SMODS.Joker:take_ownership('j_burglar', {
-    
-})
-
-SMODS.Joker:take_ownership('j_cavendish', {
-    
-})
-
-SMODS.Joker:take_ownership('j_vampire', {
-    
-})
-
-SMODS.Joker:take_ownership('j_midas_mask', {
-    
-})
-
-SMODS.Joker:take_ownership('j_campfire', {
-    
+	atlas = 'JokersDDB',
+	pos = { x = 8, y = 0 },
 })
 
 SMODS.Joker:take_ownership('j_smiley', {
-    
+    atlas = 'JokersDDB',
+	pos = { x = 7, y = 0 },
+})
+
+SMODS.Joker:take_ownership('j_campfire', {
+    atlas = 'JokersDDB',
+	pos = { x = 6, y = 0 },
+})
+
+SMODS.Joker:take_ownership('j_luchador', {
+    atlas = 'JokersDDB',
+	pos = { x = 5, y = 0 },
+})
+
+SMODS.Joker:take_ownership('j_midas_mask', {
+    atlas = 'JokersDDB',
+	pos = { x = 4, y = 0 },
+})
+
+SMODS.Joker:take_ownership('j_vampire', {
+    atlas = 'JokersDDB',
+	pos = { x = 3, y = 0 },
+})
+
+SMODS.Joker:take_ownership('j_burglar', {
+    atlas = 'JokersDDB',
+	pos = { x = 2, y = 0 },
+})
+
+SMODS.Joker:take_ownership('j_cavendish', {
+    atlas = 'JokersDDB',
+	pos = { x = 1, y = 0 },
+})
+
+SMODS.Joker:take_ownership('j_gros_michel', {
+    atlas = 'JokersDDB',
+	pos = { x = 0, y = 0 },
 })
 
 ----------------------------------------------------------Planets----------------------------------------------------------
